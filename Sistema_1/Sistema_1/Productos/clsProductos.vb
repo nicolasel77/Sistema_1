51e46de8e975fa3ba45d9220a51cd7f080823593
Nicolas <nicolasel77@hotmail.com>
2020-07-09 20:06:33 -0300

    Update clsProductos.vb

--- Sistema_1/Sistema_1/Productos/clsProductos.vb
+++ Sistema_1/Sistema_1/Productos/clsProductos.vb
@@ -2,13 +2,13 @@
 
 Public Class clsProductos
 
+#Region " Devolver Datos "
     Public Function Datos() As DataTable
-        Dim srProd As New StreamReader(Application.StartupPath + "\Productos.txt")
-
         Dim dt As New DataTable()
         dt.Columns.Add("Id", GetType(Integer))
         dt.Columns.Add("Nombre", GetType(String))
 
+        Dim srProd As New StreamReader(Application.StartupPath + "\Productos.txt")
         Dim vlinea As String = srProd.ReadLine
 
         While vlinea IsNot Nothing
@@ -18,8 +18,26 @@ Public Class clsProductos
             vlinea = srProd.ReadLine
 
         End While
+        srProd.Close()
 
         Return dt
 
     End Function
+
+#End Region
+
+#Region " Editar Datos"
+    Public Sub Agregar()
+
+    End Sub
+
+    Public Sub Editar()
+
+    End Sub
+
+    Public Sub Borrar()
+
+    End Sub
+
+#End Region
 End Class
